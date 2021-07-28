01f090937a7c0b909ce5ca90a5859df09d2328b1
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-07-28 11:16:27 -0400

    add Xml mixin with `schema_doc` method

---
+++ lib/xmltools/xml.rb
@@ -0,0 +1,21 @@
+require 'nokogiri'
+require 'xmltools'
+
+module Xmltools
+  # Mixin methods for working with XML
+  module Xml
+    class InvalidSchemaError < StandardError; end
+    
+    def xml_files
+      @xml_files ||= Dry::Files.new
+    end
+    
+    def schema_doc(path)
+      begin
+        Nokogiri::XML::Schema(xml_files.read(path))
+      rescue Nokogiri::XML::SyntaxError => e
+        raise InvalidSchemaError, e.message
+      end
+    end
+  end
+end
